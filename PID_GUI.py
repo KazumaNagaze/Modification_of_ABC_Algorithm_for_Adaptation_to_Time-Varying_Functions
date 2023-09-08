d23d5401af932b75687c250b8ba94c3cbd20c76f
{KazumaNagase} <{k4zum4.n4g4se@icloud.com}>
2023-09-08 12:57:21 +0900

    パラメータをアナログ化

--- PID_GUI.py
+++ PID_GUI.py
@@ -3,52 +3,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tkinter import ttk
 import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-# PIDパラメータの初期値
+# 初期PIDパラメータの設定
 Kp = 1.0
 Ki = 1.0
 Kd = 1.0
 
-# PIDパラメータを設定する関数
-def set_pid_parameters():
-    global Kp, Ki, Kd
+# シミュレーション用時間設定
+time = np.linspace(0, 10, 1000)
+
+# 制御対象のシステムモデル（二次遅れ系）
+omega_n = 2.0  # 自然角周波数
+zeta = 1.5     # 減衰比（1より大きい）
+system = ctrl.TransferFunction([omega_n**2], [1, 2*zeta*omega_n, omega_n**2])
+
+# グラフデータの初期化
+input_signal = np.ones_like(time)
+response = np.zeros_like(time)
+
+# パラメータ変更時のコールバック関数
+def parameter_changed(event):
+    global Kp, Ki, Kd, input_signal, response
+
+    # パラメータを更新
     Kp = Kp_scale.get()
     Ki = Ki_scale.get()
     Kd = Kd_scale.get()
 
-# シミュレーションを実行する関数
-def simulate():
-    set_pid_parameters()
-
-    # シミュレーション時間
-    time = np.linspace(0, 10, 1000)
-
-    # 制御対象のシステムモデル（二次遅れ系）
-    omega_n = 2.0      # 自然角周波数
-    zeta = 1.5         # 減衰比（1より大きい）
-    system = ctrl.TransferFunction([omega_n**2], [1, 2*zeta*omega_n, omega_n**2])
-
-    # PID制御器
-    controller = ctrl.TransferFunction([Kd, Kp, Ki], [1, 0])
-
-    # 制御系を組み立て
+    # 制御器を再構築
+    controller = ctrl.TransferFunction([Kd, Ki, Kp], [1, 0])
     closed_loop_system = ctrl.feedback(controller * system, 1)
 
-    # ステップ入力信号
-    input_signal = np.ones_like(time)
-
     # 制御系の応答を計算
-    time, response = ctrl.step_response(closed_loop_system, time)
+    _, response = ctrl.step_response(closed_loop_system, time)
 
-    # プロット
-    plt.figure()
-    plt.plot(time, input_signal, label="目標値")
-    plt.plot(time, response, label="応答")
-    plt.xlabel("時間")
-    plt.ylabel("値")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # グラフを更新
+    update_graph()
+
+# グラフを更新する関数
+def update_graph():
+    ax.clear()
+    ax.plot(time, input_signal, label="目標値")
+    ax.plot(time, response, label="応答")
+    ax.set_xlabel("時間")
+    ax.set_ylabel("値")
+    ax.legend()
+    ax.grid(True)
+    canvas.draw()
 
 # Tkinterウィンドウの設定
 root = tk.Tk()
@@ -63,24 +65,26 @@ Kp_label.grid(row=0, column=0, padx=5, pady=5)
 Kp_scale = tk.Scale(pid_frame, from_=0.0, to=10.0, resolution=0.01, orient="horizontal")
 Kp_scale.grid(row=0, column=1, padx=5, pady=5)
 Kp_scale.set(Kp)
+Kp_scale.bind("<Motion>", parameter_changed)  # スライダーが動くたびにパラメータを更新
 
 Ki_label = ttk.Label(pid_frame, text="Ki:")
 Ki_label.grid(row=1, column=0, padx=5, pady=5)
 Ki_scale = tk.Scale(pid_frame, from_=0.0, to=10.0, resolution=0.01, orient="horizontal")
 Ki_scale.grid(row=1, column=1, padx=5, pady=5)
 Ki_scale.set(Ki)
+Ki_scale.bind("<Motion>", parameter_changed)  # スライダーが動くたびにパラメータを更新
 
 Kd_label = ttk.Label(pid_frame, text="Kd:")
 Kd_label.grid(row=2, column=0, padx=5, pady=5)
 Kd_scale = tk.Scale(pid_frame, from_=0.0, to=10.0, resolution=0.01, orient="horizontal")
 Kd_scale.grid(row=2, column=1, padx=5, pady=5)
 Kd_scale.set(Kd)
+Kd_scale.bind("<Motion>", parameter_changed)  # スライダーが動くたびにパラメータを更新
 
-update_button = ttk.Button(pid_frame, text="パラメータ更新", command=set_pid_parameters)
-update_button.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
-
-# シミュレーションボタン
-simulate_button = ttk.Button(root, text="シミュレーション実行", command=simulate)
-simulate_button.grid(row=1, column=0, padx=10, pady=10)
+# グラフの初期化
+fig, ax = plt.subplots(figsize=(6, 4))
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvas.get_tk_widget().grid(row=0, column=1, padx=10, pady=10)
+update_graph()
 
 root.mainloop()
